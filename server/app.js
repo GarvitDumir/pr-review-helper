47da778e16b0939536342528d3d1f453473e99d7
GarvitDumir <garvit.dumir1103@gmail.com>
2026-06-03 21:47:40 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/review", reviewRoute);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
